8205737b49cad04596c1ce8135c9aa6e1a33651f
Ajiniyaz <scorpionskilful@gmail.com>
2023-08-15 22:36:41 +0500

    sixth task

--- 8 Object/1/6.js
+++ 8 Object/1/6.js
@@ -1,45 +1,68 @@
-// В анкете заполняется информация о семи сотрудниках, принятых на работу на основе почасовой оплаты:
-// фамилия, оплата за час, дата начала работы, дата окончания. Напишите сценарий,
-// при работе которого определяется:
-// - количество рабочих дней для каждого сотрудника;
-// - количество выходных дней между заданными датами;
-// - размер гонорара, учитывая, что каждый рабочий день длится 8 часов
-// (стоимость одного часа работы указана в договоре и содержится в анкете)
+// В анкете заполняется информация о семи сотрудниках: фамилия, дата заключения контракта,
+// продолжительность отпуска. Ровно через одиннадцать месяцев после начала работы сотрудникам
+// предоставляется отпуск, продолжительность которого оговорена в контракте (12, 24, 30, 48, 60 дней).
+// Напишите сценарий, определяющий:
+// - дату начала и окончания отпуска для каждого из сотрудников;
+// - фамилии сотрудников, начала отпусков которых приходятся на заданный месяц
+// (месяц выбирается пользователем);
+// - в зависимости от введенной пользователем даты фамилии сотрудников, которым предоставлен отпуск;
+// - в зависимости от введенной пользователем даты фамилии сотрудников, которые в данный момент
+// работают (не находятся в отпуске).
 
-// Anketa saatlıq mıynet haqı tiykarında jumısqa qabıl etilgen jeti dana xızmetker tuwrısındaǵı maǵlıwmatlar berilgen :
-// famılıya, saatına tólew, baslanıw sánesi, tawısıw sánesi. Skript jazıń jumıs dawamında belgilenedi:
-// - hár bir xızmetker ushın jumıs kúnleri sanı ;
-// - berilgen sáneler arasındaǵı dem alıw kúnleri sanı ;
-// - hár bir jumıs kúni 8 saat dawam etiwin esapqa alǵan halda tólew muǵdarı;
-// (bir saatlıq jumıstıń bahası shártnamada kórsetilgen hám sorawnamada keltirilgen);
+// Anketada jeti xızmetker haqqında maǵlıwmatlar berilgen: famılıyası, shártnama dúzilgen sánesi,demalıstıń dawam etiw waqtı. Jumıs baslanǵanınan anıq on bir ay ótkennen, xızmetkerlerge demalıs beriledi, onıń múddeti shártnamada kórsetilgen (12, 24, 30, 48, 60 kún).
+// Tómendegilerdi belgileytuǵın skript jazıń :
+// - hár bir xızmetker ushın demalıstıń baslanıwı hám tawısıw sánesi;
+// - demalısları málim bir ayda baslanatuǵın xızmetkerlerdiń famılıyaları
+// (ay paydalanıwshı tárepinen saylanadı );
+// - paydalanıwshı tárepinen kiritilgen sánege qaray, demalıs berilgen xızmetkerlerdiń famılıyası ;
+// - paydalanıwshı tárepinen kiritilgen sánege qaray, házir bolǵan xızmetkerlerdiń famılıyası
+// jumıs (demalısta emes).
 
 let employees = {
-    surname: ['Atabek','Amirxan','Eldar','Karamat','Islam','Muslim','Timur'],
-    beginDate:  ['2018-10-02',
-    '2018-09-02',
-    '2016-09-04',
-    '2019-03-07',
-    '2018-09-09',
-    '2018-09-01',
-    '2019-09-05',],
-    endDate:  ['2018-12-02',
-    '2018-10-02',
-    '2016-11-04',
-    '2019-05-07',
-    '2018-10-09',
-    '2018-12-01',
-    '2019-10-05',],
-    money: [5,4,5,6,3,5,3]
+  name: [
+    "Atabek",
+    "Amirxan",
+    "Eldar",
+    "Karamat",
+    "Islam",
+    "Muslim",
+    "Timur",
+  ],
+  beginDate: [
+    "2018-10-02",
+    "2018-09-02",
+    "2016-09-04",
+    "2019-03-07",
+    "2018-09-09",
+    "2018-09-01",
+    "2019-09-05",
+  ],
+  vacation: [24, 12, 30, 60, 48, 24, 60]
 };
-
-employees.surname.forEach((item,i)=>{
+let month = 9;
+let vacationStart = [], onvacation = [], worked = [];
+employees.name.forEach((item,i) => {
     let start = new Date(employees.beginDate[i]);
-    let end = new Date(employees.endDate[i]);
-    let workDay = 0, offDay = 0;
-    while (end > start) {
-        start.setDate(start.getDate()+1);
-        if (start.getDay() == 0 || 6 == start.getDay()) offDay++
-        else workDay++
+    //1
+    start.setMonth(start.getMonth()+11)
+    // console.log(start);
+    //2
+    let endDate = new Date(start);
+    endDate.setDate(endDate.getDate()+employees.vacation[i]);
+    // console.log(endDate);
+    //3
+    if (start.getMonth() == month-1) {
+        vacationStart.push(employees.name[i]);
     }
-    console.log(`Сотрудник: ${employees.surname[i]}, Количество рабочих дней: ${workDay}, Количество выходных дней: ${offDay}, Сумма $: ${employees.money[i]*workDay*8}`);
-})
+    //4 
+    if ((month-1) >= start.getMonth() && endDate.getMonth() >= (month-1)) {
+        onvacation.push(employees.name[i])
+    }else{
+        worked.push(employees.name[i])
+    }
+    
+    console.log(`сотрудник ${employees.name[i]} начала отпуска ${start.toLocaleDateString()}, окончания отпуска ${endDate.toLocaleDateString()}`);
+});
+console.log(`сотрудников, начала отпусков которых ${vacationStart}`);
+console.log(`Сейчас на работе: ${worked}`);
+console.log(`Сейчас в отпуске: ${onvacation}`);

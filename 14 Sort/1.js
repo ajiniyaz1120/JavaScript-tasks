c0253aae07b6cc2a8460ee36bd71aa039b10d92c
Ajiniyaz <scorpionskilful@gmail.com>
2023-09-28 15:55:16 +0500

    Sort first task

--- 14 Sort/1.js
+++ 14 Sort/1.js
@@ -1,3 +1,5 @@
+//Пузырьковая сортировка
+
 // let arr = filterArray(['10', true, undefined, null , 9, 15, 2]);
 
 // console.log(bubbleSort(arr));
@@ -23,26 +25,44 @@
 //     return arr.filter((val) => (typeof (val) != 'boolean' && val == Number(val))).map((val) => Number(val));
 // }
 
+//Пузырьковая сортировка
+// function BubbleSort(arr){
+//     for (let i = 0; i < arr.length; i++) {
+//         for (let j = 0; j < arr.length - 1 - i; j++) {
+//             if (arr[j] > arr[j+1]) {
+//                 let tmp = arr[j];
+//                 arr[j] = arr[j+1];
+//                 arr[j+1]=tmp
+//             }            
+//         }        
+//     }
+//     console.log(arr);
+// }
+
+// let numArray = [23,54,1,3,6,8,5,]
+
+// BubbleSort(numArray)
 
 
 
 
-function bubbleSort(arr){
-    for(let i = 0; i < arr.length; i++){
-        for (let j = 0; j < (arr.length - i - 1); j++) {
-            if (arr[j] > arr[j + 1]) {
-                
-            }
+
+//Пузырьковая сортировка
+function BubbleSort(arr){
+    let swapped 
+    do {
+        swapped = false;
+        for (let i = 0; i < arr.length - 1; i++) {
+            if (arr[i] > arr[i+1]) {
+                let temp = arr[i];
+                arr[i] = arr[i+1];
+                arr[i+1] = temp;
+                swapped = true;
+            }            
         }
-    }
-
-    console.log(arr);
+    } while (swapped);
 }
 
-let arr2 = [234, 43 , 55, 63, 5, 6, 235, 547];
-
-bubbleSort(arr2)
-
-
-
-
+let arr1 = [28,3,6,85,12,74,];
+BubbleSort(arr1);
+console.log(arr1);
